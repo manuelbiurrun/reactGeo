6c04ea60115c7a86c4ee0bd1d6312bca45e546ee
Manuel <manu.biurrun@gmail.com>
2021-09-02 16:18:32 -0300

    initial commit

---
+++ geoProyect/src/main/java/com/test/geo/geoProyect/GeoProyectApplication.java
@@ -0,0 +1,13 @@
+package com.test.geo.geoProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoProyectApplication.class, args);
+	}
+
+}
