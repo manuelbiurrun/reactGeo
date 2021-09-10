32c8addb0f8a2ed021cbcde6c3505d0711b3d94f
Manuel <manu.biurrun@gmail.com>
2021-09-10 13:09:03 -0300

    "terminado", sin probar, esperar errores varios

--- geoapp/src/components/mapComponent.js
+++ geoapp/src/components/mapComponent.js
@@ -1,49 +1,128 @@
-import axios from "axios";
-import react, {Component} from "react";
-import coordenadaService from "../services/coordenadaService";
+import React from "react";
+import {useQuery, useMutation, queryCache} from "react-query";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import { formatRelative } from "date-fns";
 
-class mapComponent extends Component {//en este componente es que uso GoogleApi
-    constructor(props) {
-        super(props)
+import mapStyles from "./mapStyles";
 
-        this.state = {
-            coordenadas: [],
-        }
-    }
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: -34.901112,
+  lng: -56.164532,
+};
 
-    componentDidMount() {
-        axios.get('http://localhost:8080/api/v1/todas').then((res) => {
-            this.setState({coordenadas: res});
-        });
-    }
-
-    enviarDatos = (event) => {
-        console.log(event)
-        //post con axios y coordenadas hardcodeadas
-        axios.post('http://localhost:8080/api/v1/agregar', {
-            latitud: 1,
-            longitud: 2
-        })
-            .then((res) => console.log(res))
-            .catch((err) => console.log(err));
-    }
-
-//cada coordenada deberia tener un id para poder iterar
-//en el return se muestra el mapa
-    render() {
-        return (
-            <form onSubmit={this.enviarDatos}>
-                <div> {this.state.coordenadas.map(coordenada => <p key = {coordenada.id}>
-                        {coordenada.lat} , {coordenada.lng}
-                </p>)}
-                </div>
-                <br />
-                <div>
-                    <button type="submit"> Agregar </button>
-                </div>
-            </form>
-        )
-    }
+async function fetchMonks() {
+    const response = await fetch("");
+    const {monks} = await response.json();
+    return monks;
 }
 
-export default mapComponent
+async function createMonks(newMonk) {
+    const response = await fetch("", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({monk: newMonk}),
+    });
+    const {monk} = await response.json();
+    return monk;
+}
+
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+  const {data: monks, error} = useQuery("monks", fetchMonks);
+  const [mutate] = useMutation(createMonks, {
+      onSuccess: (newMonk) => {
+          queryCache.setQueryData("monks", (current) => [...current, newMonk])
+      },
+  });
+
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    mutate({
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+    })
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  if (loadError) return <span>Load Error</span>;
+  if(error) return <span>Query Error</span>;  
+  if (!isLoaded) return <span>...Loading...</span>;
+
+  return (
+    <div>
+      <h1>
+        FoodMonks{" "}
+        <span role="img" aria-label="tent">
+          ⛺️
+        </span>
+      </h1>
+
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {monks.map((monk) => (
+          <Marker
+            key={`${monk.lat}-${monk.lng}`}
+            position={{ lat: monk.lat, lng: monk.lng }}
+            onClick={() => {
+              setSelected(monk);
+            }}
+            icon={{
+              url: `/bear.svg`,
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+              scaledSize: new window.google.maps.Size(30, 30),
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>
+                <span role="img" aria-label="bear">
+                  🐻
+                </span>{" "}
+                Alert
+              </h2>
+              <p>Spotted {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+    </div>
+  );
+}
